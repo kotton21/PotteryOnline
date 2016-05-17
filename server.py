894999991c11357f564903ca5e4787d7bc754815
Karl Bayer <kotton21@gmail.com>
2016-05-17 23:13:20 +0000

    possible add server? attempts at logging

--- server.py
+++ server.py
@@ -4,9 +4,12 @@ import SimpleHTTPServer
 import SocketServer
 import PotGenerator
 import os
+import logging
+import logging.handlers
+import sys
+import time
 
 os.chdir('/home/pi/PotteryOnline/')
-
 
 PORT = 8080
 
@@ -27,10 +30,37 @@ class MySimpleHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 			finally:
 				f.close()
 
+#logging from http://blog.scphillips.com/posts/2013/07/getting-a-python-script-to-run-in-the-background-as-a-service-on-boot/
+LOG_FILENAME = "./logs/server.log"
+LOG_LEVEL = logging.INFO
+logger = logging.getLogger(__name__)
+logger.setLevel(LOG_LEVEL)
+logHandler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when="midnight", backupCount=7)
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(messages)s')
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
+
+class MyLogger(object):
+	def __init__(self, logger, level):
+		"""Needs a logger and a Logger level."""
+		self.logger = logger
+		self.level = level
+	def write(self, message):
+		if message.rstrip() != "":
+			self.logger.log(self.level, message.rstrip())
+
+sys.stdout = MyLogger(logger, logging.INFO)
+#sys.stderr = MyLogger(logger, logging.ERROR)
+#end logging
+#usage: logger.info("bla bla") or print "..."
+
+logger.info("starting server")
+
 Handler = MySimpleHTTPRequestHandler
 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
+logger.info("serving at port" + str(PORT))
 print "serving at port", PORT
 httpd.serve_forever()
 
